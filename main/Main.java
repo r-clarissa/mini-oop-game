94f3fab7e1aeaed333ca9fb12786483c9bbef53b
Clarissa G. Rodriguez <70369183+r-clarissa@users.noreply.github.com>
2023-01-31 15:44:19 +0800

    Updated documentation

--- main/Main.java
+++ main/Main.java
@@ -2,11 +2,6 @@
 This is a mini shooting game that involves a shooter and zombies. Throughout the game, power
 ups spawns in random locations. This program also integrates the OOP concepts learned throughout
 the semester.
-
-@ authors:
-	Rodriguez, Clarissa G.
-	Sotto, Kyle S.
-
 created date:
 2021-12-1  18:50
 */
